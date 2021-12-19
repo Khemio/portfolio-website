5b3f1b13e77048e64014a63f1d7331cf9a00c178
Khemio <34317284+Khemio@users.noreply.github.com>
2021-12-19 16:32:59 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -73,3 +73,61 @@ gsap.from('.footer-anim', {
     duration: 1,
     ease: 'back.out(1.7)'
 }, '-=2')
+
+const openModalBtns = document.querySelectorAll('[data-modal-target]');
+const closeModalBtns = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+
+openModalBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget);
+        openModal(modal);
+    })
+})
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active');
+    modals.forEach(modal => {
+        closeModal(modal);
+    })
+})
+
+closeModalBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal');
+        closeModal(modal);
+    })
+})
+
+function openModal(modal) {
+    if(modal == null) return;
+
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+    if(modal == null) return;
+
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
+
+const inputs = document.querySelectorAll('.input');
+
+function focusFunc() {
+    let parent = this.parentNode;
+    parent.classList.add('focus');
+}
+
+function blurFunc() {
+    let parent = this.parentNode;
+    if(this.value == '') {
+        parent.classList.remove('focus');
+    }
+}
+
+inputs.forEach(input => {
+    input.addEventListener('focus', focusFunc);
+    input.addEventListener('blur', blurFunc);
+})
